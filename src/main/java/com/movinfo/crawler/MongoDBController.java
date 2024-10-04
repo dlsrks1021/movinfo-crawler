0fc7f9d870f3f35794f85d5d39c83e6c80f2959d
dlsrks1021 <dlsrks1021@gmail.com>
2024-10-04 19:41:15 +0900

    fix: set ttl index to movies collection (#15)

--- src/main/java/com/movinfo/crawler/MongoDBController.java
+++ src/main/java/com/movinfo/crawler/MongoDBController.java
@@ -1,8 +1,15 @@
 package com.movinfo.crawler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.IndexOptions;
 
 public class MongoDBController {
 
@@ -15,5 +22,21 @@ public class MongoDBController {
 
     public MongoDatabase getMongoDatabase(String db){
         return mongoClient.getDatabase(db);
+    }
+
+    public void checkAndSetTTLIndex(MongoCollection<Document> collection){
+        boolean indexExists = false;
+        List<Document> indexes = collection.listIndexes().into(new ArrayList<>());
+        for (Document index : indexes) {
+            if (index.get("key").equals(new Document("expireAt", 1))) {
+                indexExists = true;
+                break;
+            }
+        }
+
+        if (!indexExists) {
+            IndexOptions indexOptions = new IndexOptions().expireAfter(0L, java.util.concurrent.TimeUnit.SECONDS);
+            collection.createIndex(new Document("expireAt", 1), indexOptions);
+        }
     }
 }
